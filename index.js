08c10e39576f5731d465dab6511b53a59afb8b68
Nikhil <snikhil@levi.com>
2024-05-29 10:14:43 -0700

    Updated

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+// index.js
 import * as core from '@actions/core';
 import * as github from '@actions/github';
- 
+
 try {
     const nameToGreet = core.getInput('name');
     console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
     console.log(`The event payload: ${payload}`);
 } catch (error) {
-    core.setFailed(error.message)
-} 
+    core.setFailed(error.message);
+}
